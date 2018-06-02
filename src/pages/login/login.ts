daf352fb85eb3a9c58e6df732b3c97bf7f88901e
Max Gomes <maxgomes92@live.com>
2018-06-02 18:55:22 -0300

    Add comments to Facebook login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -80,13 +80,18 @@ export class LoginPage {
 
   async facebookLogin() {
     try {
+      // Log in to Facebook and request user data
       let facebookResponse = await this.facebook.login(['public_profile', 'email']);
       let facebookAuthData = {
         id: facebookResponse.authResponse.userID,
         access_token: facebookResponse.authResponse.accessToken,
       };
+
+      // Request the user from parse
       let toLinkUser = new Parse.User();
       let user = await toLinkUser._linkWith('facebook', {authData: facebookAuthData});
+
+      // If user did not exist, updates its data
       if (!user.existed()) {
         let userData = await this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture)', []);
         user.set('username', userData.name);
@@ -94,8 +99,11 @@ export class LoginPage {
         user.set('email', userData.email);
         await user.save();
       }
+
       this.navCtrl.setRoot('HomePage');
     } catch (err) {
+      console.log('Error logging in', err);
+
       this.toastCtrl.create({
         message: err.message,
         duration: 2000
